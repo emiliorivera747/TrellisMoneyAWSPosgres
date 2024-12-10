e6894c4f219ad70e9613d2db36796499d9e08fea
Emilio Rivera <emiliorivera747@gmail.com>
2024-12-10 09:01:45 -0800

    Added client side API's for Plaid API's

--- app/api/plaid/investments/holdings/route.ts
+++ app/api/plaid/investments/holdings/route.ts
@@ -3,29 +3,28 @@ import { InvestmentsHoldingsGetRequest } from "plaid";
 import { plaidClient } from "@/config/plaidClient";
 
 export async function GET(req: NextRequest) {
-    // const { access_token } = await req.json();
-    const access_token = "access-sandbox-fa4c5ca6-d883-4191-a7b2-d2c3b2e8143a";
+  // const { access_token } = await req.json();
+  const access_token = "access-sandbox-fa4c5ca6-d883-4191-a7b2-d2c3b2e8143a";
 
-    const request: InvestmentsHoldingsGetRequest = {
-        access_token: access_token,
-    };
+  const request: InvestmentsHoldingsGetRequest = {
+    access_token: access_token,
+  };
 
-    try {
-        console.log("Access Token ", access_token);
-        
-        const response = await plaidClient.investmentsHoldingsGet(request);
-        console.log("Response Data ", response.data);
-        console.log("Response Data ", response.data);
-        const holdings = response.data.holdings;
-        const securities = response.data.securities;
-        console.log("Holdings Data ", holdings);
-        console.log("Securities Data ", securities);
-        return NextResponse.json({ holdings, securities }, { status: 200 });
-    } catch (error) {
-        console.log(error.response);
-        return NextResponse.json(
-            { error: "Error fetching investment holdings data" },
-            { status: 500 }
-        );
-    }
+  try {
+    console.log("Access Token ", access_token);
+
+    const response = await plaidClient.investmentsHoldingsGet(request);
+    console.log("Response Data ", response.data);
+    console.log("Response Data ", response.data);
+    const holdings = response.data.holdings;
+    const securities = response.data.securities;
+    console.log("Holdings Data ", holdings);
+    console.log("Securities Data ", securities);
+    return NextResponse.json({ holdings, securities }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error fetching investment holdings data" },
+      { status: 500 }
+    );
+  }
 }
