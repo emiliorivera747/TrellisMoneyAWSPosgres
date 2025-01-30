9963495025b4e6478a85c839a8e3350cfe06a696
Emilio Rivera <emiliorivera747@gmail.com>
2025-01-29 18:10:29 -0800

    Added padding to the navigation bar

--- components/nav-bars/NavBar.tsx
+++ components/nav-bars/NavBar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const NavBar = () => {
   return (
-    <nav className="flex justify-between p-4 border-b border-gray-200 mx-10">
+    <nav className="flex justify-between p-5 border-b border-gray-200 mx-10">
       <Link href="/" className="flex h-full">
         {/* <img src="/logo.png" alt="Trellis Money Logo" className="h-10 mr-4" /> */}
         <span className="text-xl font-bold">Trellis Money</span>
