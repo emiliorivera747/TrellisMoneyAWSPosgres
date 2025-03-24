4b087d928d4da1aec8d00c758414d2bc7d345ce8
Emilio Rivera <emiliorivera747@gmail.com>
2025-03-23 18:42:00 -0700

    Changes sizes of accounts, investmentss, and investments-goal page.

--- app/investments/page.tsx
+++ app/investments/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <section className="h-screen">
+    <header className="mt-8 px-4"><h1 className="text-xl">Investment goals</h1></header>
+    </section>
   )
 }
 
