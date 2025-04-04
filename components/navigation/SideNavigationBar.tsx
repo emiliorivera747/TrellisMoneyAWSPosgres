02034ccdf7e05e619a58c2685b4026e845d495ae
Emilio Rivera <emiliorivera747@gmail.com>
2025-04-04 13:19:23 -0700

    Added margin to side navigation bar

--- components/navigation/SideNavigationBar.tsx
+++ components/navigation/SideNavigationBar.tsx
@@ -1,36 +1,24 @@
 "use client";
 import { usePathname } from "next/navigation";
 
-import Link from "next/link";
-
 // Icons
 import SideNavItemLink from "@/components/navigation/SideNavItemLink";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 //Data
 import { navigationItems } from "@/utils/data/navigation-bar-data/navigationItems";
 
-// Hooks
-import useFetchUser from "@/utils/hooks/user/useFetchUser";
 
 import UserProfileAvatarMenu from "@/features/user-profile/components/UserProfileAvatarMenu";
 
-import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-} from "@/components/ui/hover-card";
-import { User } from "lucide-react";
 
 const SideNavigationBar: React.FC = () => {
-  const { user } = useFetchUser();
 
   const pathname = usePathname();
   const currentPath = pathname;
 
   return (
     <aside className=" sm:border-tertiary-200 flex flex-col sm:flex-row justify-start w-full sm:justify-center sm:w-24 2xl:w-48  sticky text-white sm:border-r border-tertiary-300 border-box h-screen">
-      <nav className=" flex flex-row sm:flex-col  pb-4 justify-between items-center">
+      <nav className=" flex flex-row sm:flex-col  pb-4 justify-between items-center my-10">
         <ul className="flex flex-row sm:flex-col w-full sm:justify-normal sm:items-start items-center">
           <li className="h-[3rem] w-[3rem] sm:mb-4 hover:bg-tertiary-300 rounded-[100%] flex items-center justify-center">
             <div className="text-tertiary-900 font-bold text-3xl">T</div>
