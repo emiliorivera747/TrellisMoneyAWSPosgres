446ec97b6f9a2bfacee61a3665980bfb748a3bce
Emilio Rivera <emiliorivera747@gmail.com>
2025-04-05 17:38:15 -0700

    fixed footer

--- components/footers/Footer.tsx
+++ components/footers/Footer.tsx
@@ -2,20 +2,20 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-white text-tertiary-800 py-6  border-tertiary-300 bottom-0 left-0 right-0text-xs border-t">
-      <div className="container h-full text-center">
-      <p className="text-xs text-tertiary-700">
-        &copy; {new Date().getFullYear()} Trellis Money. All rights reserved.
-      </p>
-      <p className="text-xs mt-2 text-tertiary-700">
-        <a href="/privacy" className="hover:text-gray-400">
-        Privacy Policy
-        </a>{" "}
-        |
-        <a href="/terms" className="hover:text-gray-400 ml-2">
-        Terms of Service
-        </a>
-      </p>
+    <footer className=" text-tertiary-800 py-6 border-tertiary-300 bottom-0 left-0 right-0 text-xs border-t ">
+      <div className="h-full text-center items-center  w-full">
+        <p className="text-xs text-tertiary-700">
+          &copy; {new Date().getFullYear()} Trellis Money. All rights reserved.
+        </p>
+        <p className="text-xs mt-2 text-tertiary-700">
+          <a href="/privacy" className="hover:text-gray-400">
+            Privacy Policy
+          </a>{" "}
+          |
+          <a href="/terms" className="hover:text-gray-400 ml-2">
+            Terms of Service
+          </a>
+        </p>
       </div>
     </footer>
   );
