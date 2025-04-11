0cba40686583af391acb433d14124463b375f535
Emilio Rivera <emiliorivera747@gmail.com>
2025-04-10 21:51:32 -0700

    exchange-token api is protected

--- app/api/plaid/exchange-token/route.ts
+++ app/api/plaid/exchange-token/route.ts
@@ -2,9 +2,20 @@ import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { createClient } from "@/utils/supabase/server";
 import { client } from "@/config/plaidClient";
+import { ItemGetResponse } from "plaid";
+import { AxiosResponse } from "axios";
+import { getItem } from "@/utils/api-helpers/plaid/getItem";
 
+/**
+ * Handles the POST request to exchange a public token for an access token
+ * and store the item in the database.
+ *
+ * @param req - The incoming request object
+ * @returns A JSON response with the access token or an error message
+ */
 export async function POST(req: NextRequest) {
   const { public_token } = await req.json();
+
   try {
     const supabase = await createClient();
 
@@ -12,42 +23,65 @@ export async function POST(req: NextRequest) {
       data: { user },
     } = await supabase.auth.getUser();
 
-    if (!user)
-      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-
+    // Exchange the public token for an access token
     const response = await client.itemPublicTokenExchange({ public_token });
     const { access_token } = response.data;
 
-    // update the access token in the database
+    // Retrieve item details using the access token
     const item = await client.itemGet({ access_token });
 
-    // If the item already exists, remove it
-    const itemFound = await prisma.item.findUnique({
-      where: {
-        item_id: item.data.item.item_id,
-      },
-    });
+    // Check if the item already exists in the database
+    const itemFound = await getItem(item.data.item.item_id);
 
+    // If the item exists, remove it from Plaid
     if (itemFound) await client.itemRemove({ access_token });
 
-    // Add the new item
-    await prisma.item.create({
-      data: {
-        item_id: item.data.item.item_id,
-        institution_id: item.data.item.institution_id || "",
-        user_id: user.id,
-        access_token,
-        request_id: item.data.request_id,
-        update_type: item.data.item.update_type,
-        consent_expiration_time: item.data.item.consent_expiration_time || "",
-      },
-    });
+    // Add the new item to the database
+    const addItemResponse = await addItem(user?.id ?? "", item, access_token);
 
-    return NextResponse.json({ access_token });
+    // Return the access token if the item was added successfully
+    if (addItemResponse) return NextResponse.json({ access_token });
+
+    // Return an error if the item could not be added
+    return NextResponse.json(
+      { error: "Error adding item to database" },
+      { status: 500 }
+    );
   } catch (error) {
+    // Handle errors during the token exchange process
     return NextResponse.json(
       { error: "Error exchanging public token" },
       { status: 500 }
     );
   }
 }
+
+/**
+ * Adds the item to the database.
+ *
+ * @param user_id - The ID of the authenticated user
+ * @param item - The item details retrieved from Plaid
+ * @param access_token - The access token associated with the item
+ * @returns The created item record or an error
+ */
+const addItem = async (
+  user_id: string,
+  item: AxiosResponse<ItemGetResponse, any>,
+  access_token: string
+) => {
+  // Create a new item record in the database
+  const res = prisma.item.create({
+    data: {
+      item_id: item.data.item.item_id,
+      institution_id: item.data.item.institution_id || "",
+      user_id: user_id,
+      access_token,
+      request_id: item.data.request_id,
+      update_type: item.data.item.update_type,
+      consent_expiration_time: item.data.item.consent_expiration_time || "",
+    },
+  });
+  return res;
+};
+
+
---
+++ utils/api-helpers/plaid/getItem.ts
@@ -0,0 +1,15 @@
+import {prisma} from '@/lib/prisma';
+
+/**
+*
+* Checks if the item already exists in the database.
+*
+*/
+export const getItem = async (item_id: string) => {
+ const item = await prisma.item.findUnique({
+   where: {
+     item_id,
+   },
+ });
+ return item;
+};
