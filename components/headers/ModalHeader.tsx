b9448a4e7914669b813c54504a0198cdaeb36257
Emilio Rivera <emiliorivera747@gmail.com>
2025-05-06 10:16:12 -0700

    Added ModalHeader

---
+++ components/headers/ModalHeader.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  AlertDialogCancel,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+
+import { cn } from "@/lib/utils";
+
+interface ModalHeaderProps {
+  title: string;
+  className?: string;
+  ref?: React.Ref<HTMLDivElement>;
+}
+
+/**
+ *
+ * Responsible for showing the header of the modal
+ *
+ * @param name - The title of the modal
+ * @returns
+ */
+const ModalHeader = ({ title, ref, className }: ModalHeaderProps) => {
+  const defaultClass =
+    "flex justify-between items-center border-b border-tertiary-200 p-2 mb-2";
+  return (
+    <header ref={ref} className={cn(defaultClass, className)}>
+      <AlertDialogTitle>{title}</AlertDialogTitle>
+      <AlertDialogCancel className="flex items-center justify-center border-none shadow-none rounded-full py-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.8}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6 18 18 6M6 6l12 12"
+          />
+        </svg>
+      </AlertDialogCancel>
+    </header>
+  );
+};
+
+export default ModalHeader;
--- features/accounts/components/AccountCard.tsx
+++ features/accounts/components/AccountCard.tsx
@@ -3,29 +3,48 @@ import React from "react";
 import { Account } from "@/types/plaid";
 
 import { FaAirbnb } from "react-icons/fa";
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+import ModalHeader from "@/components/headers/ModalHeader";
 
 const AccountCard = ({ account }: { account: Account }) => {
   return (
-    <div
-      key={account.account_id}
-      
-      className="border rounded-[12px] grid grid-cols-3 mb-4  border-tertiary-200 hover:shadow-lg transition duration-500 ease-in-out p-3"
-    >
-      <div
-        className="flex flex-col w-[8rem] h-[4rem] bg-gradient-to-r from-primary-500 to-primary-800 rounded-[8px] items-center justify-center text-white py-1 pb-2"
-      >
-        <span className="text-white text-[0.6rem] self-end pr-2">0000</span>
-        <FaAirbnb size={100} />
-      </div>
-      <div className="flex items-center justify-start font-medium text-[0.9rem] text-tertiary-1000">
-        {account.name}
-      </div>
-      <div className="flex items-center justify-end font-light text-tertiary-800 pr-4">
-        {account?.balance?.current
-          ? "$" + Number(account.balance.current).toFixed(2)
-          : "$0.00"}
-      </div>
-    </div>
+    <AlertDialog>
+      <AlertDialogTrigger className="w-full">
+        {" "}
+        <div
+          key={account.account_id}
+          className="border rounded-[12px] grid grid-cols-3 mb-4  border-tertiary-200 hover:shadow-lg transition duration-500 ease-in-out p-3 w-full"
+        >
+          <div className="flex flex-col w-[8rem] h-[4rem] bg-gradient-to-r from-primary-500 to-primary-800 rounded-[8px] items-center justify-center text-white py-1 pb-2">
+            <span className="text-white text-[0.6rem] self-end pr-2">0000</span>
+            <FaAirbnb size={100} />
+          </div>
+          <div className="flex items-center justify-start font-medium text-[0.9rem] text-tertiary-1000">
+            {account.name}
+          </div>
+          <div className="flex items-center justify-end font-light text-tertiary-800 pr-4">
+            {account?.balance?.current
+              ? "$" + Number(account.balance.current).toFixed(2)
+              : "$0.00"}
+          </div>
+        </div>
+      </AlertDialogTrigger>
+      <AlertDialogContent className="h-[70vh] w-[60vw] overflow-scroll max-w-screen rounded-[12px]">
+        <AlertDialogHeader>
+          <ModalHeader title={account.name} />
+          <AlertDialogDescription></AlertDialogDescription>
+        </AlertDialogHeader>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 };
 
