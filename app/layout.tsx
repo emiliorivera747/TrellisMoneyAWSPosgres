bde7615cb88d20aee8354de46debaa7993ce58cf
Emilio Rivera <emiliorivera747@gmail.com>
2025-07-07 18:27:42 -0700

    removed script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/theme-provider";
-import Script from "next/script";
-
 
 //External Libraries
 import "react-toastify/dist/ReactToastify.css";
