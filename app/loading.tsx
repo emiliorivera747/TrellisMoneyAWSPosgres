9e2d6cbc4fd4f814a038fd46266ac2ff646abd78
Emilio Rivera <emiliorivera747@gmail.com>
2025-09-05 21:35:57 -0700

    Added Home page skeleton

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import HomePageSkeleton from '@/components/skeletons/pages/HomePageSkeleton';
 
+/**
+ * Home page skeleton
+ * 
+ */
 const Loading: React.FC = () => {
     return (
         <HomePageSkeleton />
