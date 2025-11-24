1db2ccb0ac6dac5bff27192d0f3681ac4106f221
Emilio Rivera <emiliorivera747@gmail.com>
2025-11-24 12:37:54 -0800

    Added steps to the modal

--- features/accounts/components/AddConnection.tsx
+++ features/accounts/components/AddConnection.tsx
@@ -7,6 +7,7 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
@@ -14,6 +15,7 @@ import {
 
 // Hooks
 import useGenerateToken from "@/hooks/plaid/useGenerateToken";
+import { useMultistepForm } from "@/hooks/forms/useMultistepForm";
 
 import { usePlaidLink } from "react-plaid-link";
 
@@ -25,6 +27,22 @@ import { usePlaidLink } from "react-plaid-link";
  */
 const AddConnection = () => {
   const linkToken = useGenerateToken();
+
+  const steps = [
+    {
+      title: "Institution",
+      description: "Securely link your bank account using Plaid",
+      content: <div className="h-full">Step 1</div>,
+    },
+    {
+      title: "Select Account Owner",
+      description: "Who owns this account?",
+      content: <div className=""> Step 2</div>,
+    },
+  ];
+
+  const { currentStep, isFirstStep, isLastStep, back, next } =
+    useMultistepForm(steps);
 
   const { open, ready, error } = usePlaidLink({
     token: linkToken,
@@ -47,19 +65,51 @@ const AddConnection = () => {
   if (error) return <p>Error loading Plaid: {error.message}</p>;
 
   return (
-    <div className=" w-[18rem] flex flex-col rounded-[12px] bg-white h-[10rem]">
+    <div className=" w-[18rem] flex flex-col rounded-[12px] bg-white h-[10rem] my-4">
       <Dialog>
         <DialogTrigger>
-          <span>{}</span>
+          <span className="w-full h-4 bg-tertiary-300 text-tertiary-900 py-4 px-6 rounded-[12px]">
+            Add Account
+          </span>
         </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Who is the owner of the account?</DialogTitle>
-            <DialogDescription className="py-6">
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </DialogDescription>
+
+        <DialogContent className="p-0 pt-4 pb-6 rounded-[12px] h-[60vh] ">
+          <DialogHeader className="border-b border-tertiary-300  flex ustify-between h-[3rem] ">
+            <DialogTitle className="pl-4 text-md font-light text-tertiary-900">
+              {currentStep.title}
+            </DialogTitle>
           </DialogHeader>
+
+          {/* Content */}
+          <div className=" h-[20rem] px-4 overflow-scroll">
+            {currentStep.description && (
+              <DialogDescription className="flex items-center pb-2">
+                {currentStep.description}
+              </DialogDescription>
+            )}
+            {currentStep.content}
+          </div>
+
+          {/* Footer */}
+            <DialogFooter className="mt-4 flex sm:justify-between px-4 h-[3.2rem] transition-transform transform duration-500 ease-in-out">
+            {!isFirstStep && (
+              <button
+                className="px-4 py-2 bg-gray-200 rounded-[12px]"
+                onClick={() => back()}
+              >
+                Previous
+              </button>
+            )}
+
+            <div className="flex justify-end w-full">
+              <button
+              className="px-4 py-2 bg-primary-700 text-white rounded-[12px]"
+              onClick={() => next()}
+              >
+              Next
+              </button>
+            </div>
+          </DialogFooter>
         </DialogContent>
       </Dialog>
     </div>
--- hooks/forms/useMultistepForm.tsx
+++ hooks/forms/useMultistepForm.tsx
@@ -1,40 +1,55 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
+
+interface Step<T = React.ReactNode> {
+  title?: string;
+  description?: string;
+  content: T;
+}
 
 interface UseMultistepFormReturn<T> {
-  goToStep: (index: number) => void;
-  next: () => void;
-  previous: () => void;
-  isLastStep: boolean;
+  currentStep: Step<T>;
+  currentIndex: number;
+  steps: Step<T>[];
   isFirstStep: boolean;
-  getStep: () => T;
+  isLastStep: boolean;
+  goTo: (index: number) => void;
+  next: () => void;
+  back: () => void;
+  setStep: (index: number) => void;
 }
 
 /**
  * Allows to you add mutiple steps fo a form
  */
-export function useMultistepForm<T>(steps: T[]): UseMultistepFormReturn<T> {
-  const [index, setIndex] = useState(0);
+export function useMultistepForm<T>(
+  steps: Step<T>[]
+): UseMultistepFormReturn<T> {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const goTo = useCallback(
+    (index: number) => {
+      if (index >= 0 && index < steps.length) {
+        setCurrentIndex(index);
+      }
+    },
+    [steps.length]
+  );
 
-  function next() {
-    setIndex((i) => {
-      if (i >= steps.length - 1) return i;
-      return i + 1;
-    });
-  }
+  const next = useCallback(() => {
+    setCurrentIndex((i) => (i >= steps.length - 1 ? i : i + 1));
+  }, [steps.length]);
 
-  function previous() {
-    setIndex((i) => {
-      if (i <= 0) return i;
-      return i - 1;
-    });
-  }
-
+  const back = useCallback(() => {
+    setCurrentIndex((i) => (i <= 0 ? i : i - 1));
+  }, []);
   return {
-    goToStep: (index: number) => setIndex(index),
+    currentStep: steps[currentIndex],
+    currentIndex,
+    steps,
+    isFirstStep: currentIndex === 0,
+    isLastStep: currentIndex === steps.length - 1,
+    goTo,
     next,
-    previous,
-    isLastStep: index === steps.length - 1,
-    isFirstStep: index === 0,
-    getStep: () => steps[index],
+    back,
+    setStep: goTo,
   };
 }
