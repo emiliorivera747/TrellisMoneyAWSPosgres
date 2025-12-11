abcfc3db6b349e49807b21b24e22815990bbef6c
Emilio Rivera <emiliorivera747@gmail.com>
2025-12-11 12:37:03 -0800

    Taking into account more events Stripe webhook

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -7,9 +7,21 @@ import { verifyWebhookSignature } from "@/utils/api-helpers/stripe/verifyWebhook
 import { NextRequest } from "next/server";
 
 /**
+ * Handles incoming POST requests for Stripe webhooks.
  *
- * Stripe webhook handler
+ * This function processes Stripe webhook events by verifying the signature,
+ * parsing the event, and handling specific event types such as:
+ * - `checkout.session.completed`: Handles successful checkout sessions.
+ * - `customer.subscription.deleted`: Handles subscription deletions.
  *
+ * If the event type is unhandled, it logs the event type for debugging purposes.
+ *
+ * @param req - The incoming `NextRequest` object containing the webhook payload and headers.
+ * @returns A `Response` object indicating the result of the webhook processing:
+ * - `200 OK` if the webhook is processed successfully.
+ * - `400 Bad Request` if there is an error, such as a missing signature or invalid payload.
+ *
+ * @throws Will return a `400 Bad Request` response if an error occurs during processing.
  */
 export async function POST(req: NextRequest) {
   try {
@@ -25,10 +37,24 @@ export async function POST(req: NextRequest) {
       case "checkout.session.completed":
         await handleCheckoutSessionCompleted(event);
         break;
-      case "checkout.session.expired":
+      case "customer.subscription.created":
+        break;
+      case "customer.subscription.updated":
         break;
       case "customer.subscription.deleted":
         await handleSubscriptionDeleted(event);
+        break;
+      case "customer.subscription.resumed":
+        break;
+      case "customer.subscription.trial_will_end":
+        break;
+      case "invoice.paid":
+        break;
+      case "invoice.payment_failed":
+        break;
+      case "invoice.payment_action_required":
+        break;
+      case "invoice.upcoming":
         break;
       case "payment_intent.succeeded":
         break;
