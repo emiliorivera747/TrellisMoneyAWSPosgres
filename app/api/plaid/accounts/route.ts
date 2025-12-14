94b850c704b7f3b01fd7d2f9a36b039d3a1ac3f0
Emilio Rivera <emiliorivera747@gmail.com>
2025-12-14 14:52:50 -0800

    removed unused imports

--- app/api/plaid/accounts/route.ts
+++ app/api/plaid/accounts/route.ts
@@ -2,7 +2,6 @@ import { NextResponse, NextRequest } from "next/server";
 import { getItemsByUserId } from "@/utils/api-helpers/prisma/itemsService";
 import { getUser } from "@/utils/api-helpers/supabase/getUser";
 import { getAccounts } from "@/utils/api-helpers/plaid/accounts/getAccountV2";
-import { noAccountsError } from "@/utils/api-helpers/errors/accountErrors";
 import { updateAccounts } from "@/utils/api-helpers/plaid/accounts/updateAccountsV2";
 import { noItemsError } from "@/utils/api-helpers/errors/itemErrors";
 import { getAccountWithItemIds } from "@/utils/api-helpers/prisma/account/accountService";
--- app/api/plaid/items/remove/route.ts
+++ app/api/plaid/items/remove/route.ts
@@ -1,10 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { ItemRemoveRequest } from "plaid"; // Adjust the import path based on your project structure
 
-
-
-
-
 export async function POST(req: NextRequest) {
   try {
     const { item_id } = await req.json();
