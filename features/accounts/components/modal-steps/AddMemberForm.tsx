dd79e69f6f1abce859d5e1cf88f4a192e13e339d
Emilio Rivera <emiliorivera747@gmail.com>
2025-12-23 11:38:21 -0800

    Modal navigation works

--- features/accounts/components/modal-steps/AddMemberForm.tsx
+++ features/accounts/components/modal-steps/AddMemberForm.tsx
@@ -14,9 +14,13 @@ import FormFieldGenerator from "@/components/form-builder/FormFieldsGenerator";
 
 // Data
 import { memberFields } from "@/features/accounts/data/MemberFormFields";
+import { useConnectionContext } from "../../context/ConnectionContext";
 
 const AddMemberForm = () => {
+  const { goToRoute } = useConnectionContext();
+
   const buttonRef = useRef(null);
+
   const form = useForm<AddMemberFormSchema>({
     resolver: zodResolver(addMemberFormSchema),
   });
@@ -24,24 +28,43 @@ const AddMemberForm = () => {
   const onSubmit = () => {};
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit || (() => {}))}
-        className="h-full flex flex-col  px-10 pt-4"
-      >
-        <h1 className="text-tertiary-700 font-light pb-2">
-          Provide member details below
-        </h1>
-        <FormFieldGenerator
-          fields={memberFields.map((field) => ({
-            ...field,
-            control: form.control as any,
-          }))}
-        />
+    <div>
+      <button className="text-xs text-tertiary-700 flex items-center justify-center font-light gap-1 hover:underline" onClick={() => goToRoute('owner')}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-3"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 19.5 8.25 12l7.5-7.5"
+          />
+        </svg>
+        Account Owner
+      </button>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit || (() => {}))}
+          className="h-full flex flex-col px-6 pt-6"
+        >
+          <h1 className="text-tertiary-700 font-light pb-2">
+            Provide member details below
+          </h1>
+          <FormFieldGenerator
+            fields={memberFields.map((field) => ({
+              ...field,
+              control: form.control as any,
+            }))}
+          />
 
-        <PrimarySubmitButton ref={buttonRef} className="mt-4"/>
-      </form>
-    </Form>
+          <PrimarySubmitButton ref={buttonRef} className="mt-4" />
+        </form>
+      </Form>
+    </div>
   );
 };
 
