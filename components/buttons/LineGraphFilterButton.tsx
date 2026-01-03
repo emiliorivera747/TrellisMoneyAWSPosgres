c410975b2b889ad0f2b26951429ba6450efd4c1d
Emilio Rivera <emiliorivera747@gmail.com>
2026-01-02 20:55:00 -0800

    Made changes to the future projection graph header

--- components/buttons/LineGraphFilterButton.tsx
+++ components/buttons/LineGraphFilterButton.tsx
@@ -13,15 +13,15 @@ const LineGraphFilterButton = ({
   return (
     <button
       onClick={onClick}
-      className={`px-1 gap-2 hover:bg-tertiary-200 transition duration-300 py-[0.7rem]  rounded-[12px] text-xs hover:text-tertiary-1000 hover:border-tertiary-200   flex flex-row justify-center items-center text-center border ${
+      className={`px-1 gap-3 hover:bg-tertiary-200 transition duration-300 py-[0.7rem]  rounded-[12px] text-xs hover:text-tertiary-1000 hover:border-tertiary-200   flex flex-row justify-center items-center text-center border ${
         isSelected
           ? ` text-tertiary-1000 font-extrabold border-tertiary-500 bg-tertiary-200`
-          : "text-tertiary-800 border-tertiary-300 "
+          : "text-tertiary-800 border-tertiary-300"
       }`}
     >
       <span
         style={{ backgroundColor: color }}
-        className={`w-[0.4rem] h-[0.4rem] rounded-full`}
+        className={`w-[0.8rem] h-[0.25rem] rounded-full`}
       ></span>
       {label}
     </button>
--- components/dashboard/MultipleValPriceChange.tsx
+++ components/dashboard/MultipleValPriceChange.tsx
@@ -65,13 +65,13 @@ const MultipleValPriceChange = ({
             />
             <div className="flex items-center justify-start gap-2  text-[0.7rem] rounded-full  w-[10rem] cursor-pointer mt-[0.2rem] ml-[0.17rem]">
               {!withInflation(line) ? (
-                <span className="w-[0.35rem] h-[0.35rem] rounded-full bg-secondary-800"></span>
+                <span className="w-[0.5rem] h-[0.2rem] rounded-full bg-secondary-800"></span>
               ) : null}
               {withInflation(line) ? (
-                <span className="w-[0.35rem] h-[0.35rem] rounded-full bg-red-600"></span>
+                <span className="w-[0.5rem] h-[0.2rem] rounded-full bg-red-600"></span>
               ) : null}
               <span className="text-tertiary-700 font-light text-[0.75rem] transition duration-300 rounded-[12px] hover:text-tertiary-1000 hover:border-tertiary-200 flex flex-row justify-center items-center text-center hover:underline">
-                {!withInflation(line) ? "With Inflation" : "Without Inflation"}
+                {!withInflation(line) ? "Not adjusted for inflation" : "Adjusted for inflation"}
               </span>
             </div>
           </div>
