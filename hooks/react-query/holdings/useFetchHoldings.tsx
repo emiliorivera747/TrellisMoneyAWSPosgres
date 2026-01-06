333f0bad9962464853f9cf271074a9cf5ab1aafc
Emilio Rivera <emiliorivera747@gmail.com>
2026-01-05 18:05:27 -0800

    Added grouped and sorted investments

--- hooks/react-query/holdings/useFetchHoldings.tsx
+++ hooks/react-query/holdings/useFetchHoldings.tsx
@@ -8,7 +8,7 @@ import { holdingService } from "@/services/holdings/holdingsService";
  */
 const useFetchHoldings = () => {
   const {
-    data: holdingshData,
+    data: holdingsData,
     isError: holdingsHasError,
     error: holdingsError,
     isLoading: holdingsLoading,
@@ -20,7 +20,7 @@ const useFetchHoldings = () => {
     refetchOnMount: false,
     refetchOnWindowFocus: false,
   });
-  return { holdingshData, holdingsError, holdingsLoading, holdingsHasError };
+  return { holdingsData, holdingsError, holdingsLoading, holdingsHasError };
 };
 
 export default useFetchHoldings;
