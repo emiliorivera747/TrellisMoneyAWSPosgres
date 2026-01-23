1575cd7089ca2c89277a422463f041255c8c9560
Emilio Rivera <emiliorivera747@gmail.com>
2026-01-22 21:32:47 -0800

    Aligned the holding headers

--- features/holding/components/headers/HoldingHeader.tsx
+++ features/holding/components/headers/HoldingHeader.tsx
@@ -9,9 +9,9 @@ import { AggregateHoldingDetails } from "@/types/api-routes/holding/holding";
  */
 const HoldingHeader = ({ holding }: { holding: AggregateHoldingDetails }) => {
   return (
-    <div className="flex flex-row gap-4 tems-center text-tertiary-1000">
+    <div className="flex flex-row gap-4 items-start text-tertiary-1000">
       <div className="font-bold text-3xl">{holding.tickerSymbol}</div>
-      <div className="text-tertiary-700 text-2xl">{holding.securityName}</div>
+      <div className="text-tertiary-700 text-3xl">{holding.securityName}</div>
     </div>
   );
 };
