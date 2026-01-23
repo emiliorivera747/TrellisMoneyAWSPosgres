653fdd88b63a718227144ae1c0c0095d87bcbdcf
Emilio Rivera <emiliorivera747@gmail.com>
2026-01-22 19:40:40 -0800

    Created very basic investment page

--- app/(admin)/investments/[id]/page.tsx
+++ app/(admin)/investments/[id]/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import useFetchAggregateHoldings from "@/hooks/react-query/holdings/useFetchAggregateHoldings";
-import { convertToMoney } from "@/utils/helper-functions/formatting/convertToMoney";
 import { use } from "react";
 import HoldingHeader from "@/features/holding/components/headers/HoldingHeader";
 import MarketValueCards from "@/features/holding/components/cards/MarketValueCards";
 import AverageCostCard from "@/features/holding/components/cards/AverageCostCard";
 import AccountWithHoldingCard from "@/features/holding/components/cards/AccountWithHoldingCard";
+import PrimaryAccountSection from "@/features/accounts/components/sections/PrimaryAccountSection";
 
 const Page = ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = use(params);
@@ -39,22 +39,24 @@ const Page = ({ params }: { params: Promise<{ id: string }> }) => {
   }
 
   return (
-    <section className="h-screen overflow-scroll">
-      <header className="mt-[3.5rem] px-8">
-        <HoldingHeader holding={holding} />
-        <div className="grid grid-cols-[22rem_22rem] gap-4 mb-10">
-          <MarketValueCards holding={holding} />
-          <AverageCostCard holding={holding} />
-        </div>
-        <div className="mb-4 font-light text-tertiary-800 text-[1.2rem] mt-6">
-          Positions
-        </div>
-        <div className="flex flex-col gap-2">
-          {holding.holdings.map((h) => (
-            <AccountWithHoldingCard holding={h} key={h.holdingId} />
-          ))}
-        </div>
-      </header>
+    <section className="h-screen mx-[2%] overflow-y-scroll no-scrollbar flex flex-row gap-8">
+      <PrimaryAccountSection>
+        <header>
+          <HoldingHeader holding={holding} />
+          <div className="grid grid-cols-2 gap-4 mb-10">
+            <MarketValueCards holding={holding} />
+            <AverageCostCard holding={holding} />
+          </div>
+          <div className="mb-4 font-light text-tertiary-800 text-[1.2rem] mt-6">
+            Positions
+          </div>
+          <div className="flex flex-col gap-2">
+            {holding.holdings.map((h) => (
+              <AccountWithHoldingCard holding={h} key={h.holdingId} />
+            ))}
+          </div>
+        </header>
+      </PrimaryAccountSection>
     </section>
   );
 };
