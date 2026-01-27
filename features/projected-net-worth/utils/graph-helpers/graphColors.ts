b0c5776d06d6c249c1e41dc6dbb0f2e33b210409
Emilio Rivera <emiliorivera747@gmail.com>
2026-01-27 00:45:11 -0800

    Added global colors to future projection graph

--- features/projected-net-worth/utils/graph-helpers/graphColors.ts
+++ features/projected-net-worth/utils/graph-helpers/graphColors.ts
@@ -1,23 +1,27 @@
 // line colors
-export const upColorLine1 = "#74b816";
-export const downColorLine1 = "#e03131";
-export const flatColorLine1 = "#495057";
+export const primaryLinePositiveTrendColor = "#74b816";
+export const primaryLineNegativeTrendColor = "#e03131";
+export const primaryLineNeutralTrendColor = "#495057";
 
-export const upColorLine2 = "#339af0";
-export const downColorLine2 = "#e03131";
-export const flatColorLine2 = "#495057";
+export const secondaryLinePositiveTrendColor = "#339af0";
+export const secondaryLineNegativeTrendColor = "#e03131";
+export const secondaryLineNeutralTrendColor = "#495057";
 
 // tag text colors
-export const tagTextUpColor = "#2f9e44";
-export const tagTextDownColor = "#e03131";
-export const tagTextFlatColor = "#495057";
+export const tagTextPositiveTrendColor = "#2f9e44";
+export const tagTextNegativeTrendColor = "#e03131";
+export const tagTextNeutralTrendColor = "#495057";
 
 // tag background colors
-export const tagBgUpColor = "#d3f9d8";
-export const tagBgDownColor = "#ffe0e0";
-export const tagBgFlatColor = "#f1f3f5";
+export const tagBackgroundPositiveTrendColor = "#d3f9d8";
+export const tagBackgroundNegativeTrendColor = "#ffe0e0";
+export const tagBackgroundNeutralTrendColor = "#f1f3f5";
 
 // subheader colors
-export const subheaderUpColor = "#74b816";
-export const subheaderDownColor = "#e03131";
-export const subheaderFlatColor = "#495057";
+export const primarySubheaderPositiveTrendColor = "#5c940d";
+export const primarySubheaderNegativeTrendColor = "#e03131";
+export const primarySubheaderNeutralTrendColor = "#495057";
+
+export const secondarySubheaderPositiveTrendColor = "#1c7ed6";
+export const secondarySubheaderNegativeTrendColor = "#e03131";
+export const secondarySubheaderNeutralTrendColor = "#495057";
